696251d114110f95ed226568987b70e8d68a9181
AnnaGrechko <ann.greczko@gmail.com>
2016-05-28 00:17:12 +0300

    goodbye page

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import cgi
 import webapp2
 
 class MainPage(webapp2.RequestHandler):
+
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 
+class GoodbyePage(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Goodbye, World.  Time to sleep.')
+
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+     ('/', MainPage),
+    ('/goodbye', GoodbyePage),
 ], debug=True)
